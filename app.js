7655df18ee21584e17c07f85f62f0776a490fe81
Oscar Miras <oscar.miras@gmail.com>
2023-03-22 18:58:40 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+function filtrarCafe(beans, prices) {
+   
+}
+console.log(filtrarCafe(3, [6, 1, 2, 9, 2]), [1, 2, 2]);
+console.log(filtrarCafe(14, [7, 3, 23, 9, 14, 20, 7]), [3,7,7,9,14]);
+
+// "Should return an empty string when budget is zero"
+console.log(filtrarCafe(0, [6, 1, 2, 9, 2]), []);
